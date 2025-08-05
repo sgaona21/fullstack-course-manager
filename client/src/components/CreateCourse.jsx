f442b0f68912e375826342ab9b6621f98cff3713
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-08-05 14:29:58 -0700

    CreateCourse logic added for form - need to add auth logic to process request

--- client/src/components/CreateCourse.jsx
+++ client/src/components/CreateCourse.jsx
@@ -1,6 +1,41 @@
-
+import { useState } from "react";
 
 const CreateCourse = () => {
+  const [newCourse, setNewCourse] = useState({
+    title: '',
+    description: '',
+    estimatedTime: '',
+    materialsNeeded: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewCourse((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const submitNewCourseData = (e) => {
+    e.preventDefault();
+
+    fetch("http://localhost:5001/api/courses", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCourse),
+    })
+      .then((res) => {
+        if (res.ok) {
+          console.log("Course created!");
+        } else {
+          console.error("Something went wrong.");
+        }
+      })
+      .catch((err) => console.error("Error:", err));
+  }
+
   return (
     <div className="wrap">
       <h2>Create Course</h2>
@@ -11,42 +46,49 @@ const CreateCourse = () => {
           <li>Please provide a value for "Description"</li>
         </ul>
       </div>
-      <form>
+      <form onSubmit={submitNewCourseData} >
         <div className="main--flex">
           <div>
-            <label for="courseTitle">Course Title</label>
-            <input id="courseTitle" name="courseTitle" type="text" value="" />
+            <label htmlFor="courseTitle">Course Title</label>
+            <input
+              id="courseTitle"
+              name="title"
+              type="text"
+              value={newCourse.title}
+              onChange={handleChange}
+            />
 
             <p>By Joe Smith</p>
 
-            <label for="courseDescription">Course Description</label>
+            <label htmlFor="courseDescription">Course Description</label>
             <textarea
               id="courseDescription"
-              name="courseDescription"
-            ></textarea>
+              name="description"
+              value={newCourse.description}
+              onChange={handleChange}>
+            </textarea>
           </div>
           <div>
-            <label for="estimatedTime">Estimated Time</label>
+            <label htmlFor="estimatedTime">Estimated Time</label>
             <input
               id="estimatedTime"
               name="estimatedTime"
               type="text"
-              value=""
+              value={newCourse.estimatedTime}
+              onChange={handleChange}
             />
 
-            <label for="materialsNeeded">Materials Needed</label>
-            <textarea id="materialsNeeded" name="materialsNeeded"></textarea>
+            <label htmlFor="materialsNeeded">Materials Needed</label>
+            <textarea
+              id="materialsNeeded"
+              name="materialsNeeded"
+              value={newCourse.materialsNeeded}
+              onChange={handleChange}
+            ></textarea>
           </div>
         </div>
-        <button className="button" type="submit">
-          Create Course
-        </button>
-        <button
-          className="button button-secondary"
-          onclick="event.preventDefault(); location.href='index.html';"
-        >
-          Cancel
-        </button>
+        <button className="button" type="submit">Create Course</button>
+        <button className="button button-secondary" type="button">Cancel</button>
       </form>
     </div>
   );
